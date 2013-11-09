d6c6051a7146acc5e799c0ab28d5b0f3589a7230
fiefioor <tfon35@gmail.com>
2013-11-09 16:41:44 +0100

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -18,10 +18,17 @@ public:
 	list();
 	~list();
 	void Add(int);
+	void Delete(int);
+	void getAmount();
+	void display_U();
+	void display_D();
+
 private:
 	int amount;	
 	pole *first, *last;
 	void AddFirst(int);
+	void AddLast(int);
+	pole* Search(int);
 };
 
 list::list()
@@ -33,25 +40,66 @@ list::list()
 
 list::~list(){;}
 
+void list::getAmount()
+{
+	cout << "Ilosc elementow listy: " << amount << endl;
+}
+
+pole* list::Search(int key)
+{
+	pole* i=first;
+	while(i!=NULL)
+	{
+		if(i->key == key)
+		{
+			return i;
+		}
+		i=i->next;
+	}
+	return NULL;
+}
+
+
 void list::AddFirst(int key)
 {
-	if(amount==NULL)
+		pole* nju = new pole;
+		nju->key=key;
+	if(amount==0)
 	{
-	pole* nju = new pole;
-	nju->key = key;
-	nju->next = NULL;
-	nju->prev = NULL;
+		first=nju;
+		last=nju;
+		nju->next=NULL;
+		nju->prev=NULL;
+	}
+	else
+	{
+		first->prev=nju;
+		nju->prev=NULL;
+		nju->next=first;
+		first=nju;
+	}
 	amount++;
+}
+
+void list::AddLast(int key)
+{
+	if(amount==1)
+	{
+		last->key=key;
+		last->prev=first;
+		last->next=NULL;
 	}
 	else
 	{
 		pole* nju = new pole;
 		nju->key = key;
-		nju->prev = NULL;
-		nju->next = first;
-		first = nju;
-		amount++;
+		last->next=nju;
+		nju->next=NULL;
+		nju->prev=last;
+		last=nju;
 	}
+
+	amount++;
 }
 
 void list::Add(int key)
@@ -61,46 +109,141 @@ void list::Add(int key)
 		AddFirst(key);
 	}
 	else
+	if(amount==1)
 	{
-		pole* currentR = first;
-		pole* currentL = NULL;
-		for(int i=0; i<amount;i++)
+		if(key > first->key) AddLast(key);
+		if(key < first->key) AddFirst(key);
+	}
+	else
+	{
+		if(key < first->key) AddFirst(key);
+		else if(key > last->key) AddLast(key);
+		else
 		{
-			if(key < currentR->key )
+			pole* nju = new pole;
+			nju->key=key;
+			pole* i=first;
+			while (i->key < key)
 			{
-				if(currentR == first)
-				{
-					AddFirst(key);
-
-				}
-
-				if (currentR == last)
-				{
-					pole* nju = new pole;
-					nju->key = key;
-					nju->prev = currentR;
-					nju->next = NULL;
-					last = nju;
-					amount++;
-				}
-				else
-				{
-					pole* nju = new pole;
-					nju->key = key;
-					nju->prev = currentL;
-					nju->next = currentR;
-					amount++;
-				}
-
+				if(i->key == key) cout << "element istnieje" << endl;
+				i=i->next;
 			}
-			currentL = currentR;
-			currentR = currentR->next;
+			nju->next=i;
+			nju->prev=i->prev;
+			i->prev->next=nju;
+			i->prev=nju;
+			amount++;
 		}
 	}
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+void list::Delete(int key)
 {
-	system("pause");
+	if(amount==0) cout << "brak elementow"<<endl;
+	if(amount==1)
+	{
+		delete first;
+		first=NULL;
+		last=NULL;
+		amount--;
+	}
+	else if(key == first->key)
+	{
+		first=first->next;
+		delete first->prev;
+		first->prev = NULL;
+		amount--;
+	}
+	else if(key == last->key)
+	{
+		last = last->prev;
+		delete last->next;
+		last->next=NULL;
+		amount--;
+	}
+	else
+	{
+		pole* right = first;
+		pole* left = NULL;
+		pole* tmp=NULL;
+		if(right!=NULL)
+		{
+			if(right->key==key)
+			{
+				left=right->prev;
+				right->next->prev=left;
+				left->next=right->next;
+				delete right;
+				amount--;
+			}
+			right=right->next;
+
+		}
+
+	}
+
+
+
 }
 
+void list::display_U()
+{
+	pole* pointer = first;
+	for(int i=0;i<amount-1;i++)
+	{
+		cout << "Klucz " << i+1 << " elementu listy : " << pointer->key << endl;
+		pointer=pointer->next;
+	}
+	cout << "Klucz ostatniego elementu listy : " << pointer->key << endl;
+
+}
+
+void list::display_D()
+{
+	pole* pointer = last;
+	for(int i=amount;i>1;i--)
+	{
+		cout << "Klucz " << i << " elementu listy : " << pointer->key << endl;
+		pointer=pointer->prev;
+	}
+	cout << "Klucz pierwszego elementu listy : " << pointer->key << endl;
+
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	list* lista = new list();
+	lista->Add(22);
+	lista->getAmount();
+	lista->Add(11);
+	lista->getAmount();
+	lista->Add(43);
+	lista->getAmount();
+	lista->Add(37);
+	lista->Add(100);
+	lista->getAmount();
+	lista->display_U();
+	lista->display_D();
+	lista->Delete(100);
+	lista->Delete(11);
+	lista->getAmount();
+	lista->display_U();
+ /*	pole* test = new pole;
+	pole* test1= new pole;
+	pole* nowe = new pole;
+
+	test->key=1;
+	test1->key=2;
+	test->next=test1;
+	test1->prev=test;
+	nowe=test;
+	cout << endl << nowe->key;
+	cout << endl;
+	nowe=nowe->next;
+
+	cout << endl << nowe->key;
+	cout << endl;
+*/
+	system("pause");
+	return 0;
+}
